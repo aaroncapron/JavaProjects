692f89772a8c6112ae5748fcd86a2c969fbb3360
Aaron <159083424+aaroncapron@users.noreply.github.com>
2024-02-05 16:53:14 -0700

    Add files via upload

---
+++ reverseString.java
@@ -0,0 +1,15 @@
+//This code will reverse user-input strings
+import java.util.Scanner;
+public class reverseString {
+	public static void main (String [] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Please type in a string: ");
+		String stringinput = input.next();
+
+		char[] resultarray = stringinput.toCharArray();
+        for (int i = resultarray.length - 1; i >= 0; i--)
+
+        System.out.print(resultarray[i]);
+	}
+}
