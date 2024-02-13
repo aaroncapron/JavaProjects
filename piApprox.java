1a917de350ca4568abc4c09bd4465b142310c1c2
Aaron <159083424+aaroncapron@users.noreply.github.com>
2024-02-12 23:02:04 -0700

    Update piApprox.java

--- piApprox.java
+++ piApprox.java
@@ -1,23 +1,23 @@
 /* Author: Aaron Capron
    Personal Projet
+   This code computes the approximate the value of π given the user-input variable
    Last Updated: 2/12/2024
-   This code computes the approximate the value of π given the user-input variable 
 */
 import java.util.Scanner;
 public class piApprox{
 	public static void main(String [] args) {
-      Scanner scanner = new Scanner(System.in);
-      int n = scanner.nextInt();
-      double piApprox = 4;
-      int temp = 1;
-      double sum = 0.0;
-
-      for (int i = 0; i < n; i++) {
-         if (i % 2 == 0) {
-            sum = sum + (double) 1 / temp;
-         }else{
-            sum = sum - (double) 1 / temp;
-         }temp = temp + 2;
+	Scanner scanner = new Scanner(System.in);
+     	int n = scanner.nextInt();
+    	double piApprox = 4;
+   	int temp = 1;
+  	double sum = 0.0;
+	
+	for (int i = 0; i < n; i++) {
+        	if (i % 2 == 0) {
+            		sum = sum + (double) 1 / temp;
+       		}else{
+            		sum = sum - (double) 1 / temp;
+         	}temp = temp + 2;
       }sum = sum * 4;
       piApprox = (double) sum;
       System.out.println("piApprox = " + piApprox);
