19d4c8850200b049da51361bbc5f70cb031dd23e
Aaron <159083424+aaroncapron@users.noreply.github.com>
2024-02-12 22:22:44 -0700

    Add files via upload

---
+++ foodReader.java
@@ -0,0 +1,29 @@
+/* Author: Aaron Capron
+   Instructor: TA Ali Nouri
+   CS 1101
+   This code reads text from a file using the scanner and prints it.
+*/
+
+//Imported libraries to make stuff work!
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class foodReader {
+
+public static void main (String [] args) {
+	//Creates a file reader to pull data from
+	//Creates a scanner to read the data into Java
+	try {
+		File newObj = new File ("food_file.txt");
+		Scanner input = new Scanner (newObj);
+`		//Goes line by line to pull data separately, first by String, then by Double. 
+		while (input.hasNextLine()) {
+			System.out.println(input.next());
+			System.out.println(input.nextDouble() - 2); // Subtracts 2 from all prices listed in the .txt file
+		}input.close(); //Closes the scanner to be polite :)
+	}catch (Exception e) {
+		System.out.println("An error has occured while reading the file.\nLet your administrator know."); 
+		//If the file cannot be found, this "catches" the error message to tell the user to tell their administrator/more-experienced individual to fix it.
+		}
+	}
+}
