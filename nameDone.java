e569999be4c38f540b78b7cf859f67a2af02e71f
Aaron <159083424+aaroncapron@users.noreply.github.com>
2024-02-12 22:50:52 -0700

    Add files via upload

---
+++ nameDone.java
@@ -0,0 +1,22 @@
+/* Author: Aaron Capron
+   Instructor: Prof. Daniel Meija
+   CS 1301
+   Last Updated: 2/12/2024
+   This program asks user for a name and prints it until "END" is printed, ending the loop
+*/
+import java.util.Scanner;
+public class nameDone{
+	public static void main(String [] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What is your name?: ");
+		String name = scanner.nextLine();
+
+		while (!name.equals ("END")) {
+			System.out.println(" ");
+			System.out.println(name);
+			System.out.println("What is your name?: ");
+			name = scanner.nextLine();
+		}System.out.println("I am done.");
+		scanner.close();
+	}
+}
